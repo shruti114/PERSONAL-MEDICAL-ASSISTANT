9bc3c434d75e0eadbb0fd42366bf2dfd7ec61fd1
shruti114 <t.g.shruti06@gmail.com>
2018-07-19 22:41:07 +0530

    initial commit

---
+++ medbot.py
@@ -0,0 +1,67 @@
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+from watson_developer_cloud import ConversationV1
+import json
+
+context = None
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    print('Received /start command')
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    print('Received /help command')
+    update.message.reply_text('Help!')
+
+
+def message(bot, update):
+    print('Received an update')
+    global context
+
+    conversation = ConversationV1(username='username',
+                                  password='password',
+                                  version='2018-02-16')
+
+    # get response from watson
+    response = conversation.message(
+        workspace_id='workspace_id',
+        input={'text': update.message.text},
+        context=context)
+    print(json.dumps(response, indent=2))
+    context = response['context']
+
+    # build response
+    resp = ''
+    for text in response['output']['text']:
+        resp += text
+
+    update.message.reply_text(resp)
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater('Token')
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, message))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
